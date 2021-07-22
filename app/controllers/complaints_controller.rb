0a448d2e8b3fa276c3fcc8597963bd381f6e3ac2
Davida Marion <davida.marion@gsa.gov>
2021-07-22 14:56:16 -0400

    Get app into cloud.gov deployable state

---
+++ app/controllers/complaints_controller.rb
@@ -0,0 +1,4 @@
+class ComplaintsController < ApplicationController
+  def index
+  end
+end
---
+++ spec/requests/complaints_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Complaints", type: :request do
+  describe "GET /index" do
+    it "returns a 200 status" do
+      get root_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the text" do
+      get root_path
+      expect(response.body).to include("OHS Complaints Tracker")
+    end
+  end
+end
