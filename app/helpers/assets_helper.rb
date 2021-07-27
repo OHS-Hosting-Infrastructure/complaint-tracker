7414625a3f4f83265da3021ddf1d246cdb9e5bb4
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-07-27 16:17:45 -0400

    Use webpack image tag helpers for banner

--- app/helpers/assets_helper.rb
+++
@@ -1,5 +0,0 @@
-module AssetsHelper
-  def uswds_asset_path(path)
-    "node_modules/uswds/dist/#{path}"
-  end
-end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,5 +13,8 @@ Turbolinks.start()
 ActiveStorage.start()
 
 import 'uswds/src/stylesheets/uswds.scss'
+import 'uswds/dist/img/icon-dot-gov.svg'
+import 'uswds/dist/img/us_flag_small.png'
+import 'uswds/dist/img/icon-https.svg'
 
 require('uswds')
