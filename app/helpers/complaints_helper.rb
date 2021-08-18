9279dec02ac06d31d100cf5ef7847f38356ef318
Davida Marion <davida.marion@gsa.gov>
2021-08-18 16:21:12 -0400

    Add sortable complaint status

---
+++ app/helpers/complaints_helper.rb
@@ -0,0 +1,27 @@
+module ComplaintsHelper
+  FORMATTED_STATUS = {
+    0 => "In Progress",
+    1 => "Closed",
+    2 => "Rec. Closure",
+    3 => "Rec. Reopening",
+    4 => "Reopened"
+  }
+
+  STATUS_SORT_ORDER = {
+    "New" => 0,
+    "Rec. Reopening" => 1,
+    "Reopened" => 2,
+    "In Progress" => 3,
+    "Rec. Closure" => 4,
+    "Closed" => 5
+  }
+
+  def status(complaint_attributes)
+    formatted_status = FORMATTED_STATUS[complaint_attributes[:status]]
+    if complaint_attributes[:creationDate] > 1.week.ago && formatted_status == "In Progress"
+      "New"
+    else
+      formatted_status
+    end
+  end
+end
