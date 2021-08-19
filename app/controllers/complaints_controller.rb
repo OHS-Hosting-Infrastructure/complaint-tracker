74b1c302130ccecad0c20620d64788ca2dee295c
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-08-19 15:31:22 -0400

    Check complaints details with pa11y

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,4 +1,6 @@
 class ComplaintsController < ApplicationController
+  before_action :check_pa11y_id, only: :show, if: -> { Rails.env.test? }
+
   def index
     @complaints = FakeData::ApiResponse.hses_issues_response[:data]
   end
@@ -6,5 +8,13 @@ class ComplaintsController < ApplicationController
   def show
     @complaint = FakeData::ApiResponse.hses_issue_response(params[:id])[:data]
     render layout: "details"
+  end
+
+  private
+
+  def check_pa11y_id
+    if params[:id] == "pa11y-id"
+      params[:id] = FakeData::ApiResponse.hses_issues_response[:data].first[:id]
+    end
   end
 end
