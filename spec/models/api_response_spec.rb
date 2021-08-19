e2b5dc298873d7f6733ad78a16d80f11cacf2cbc
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-08-19 10:37:33 -0400

    Add a details api to FakeData

--- spec/models/api_response_spec.rb
+++ spec/models/api_response_spec.rb
@@ -18,13 +18,40 @@ RSpec.describe FakeData::ApiResponse do
     end
   end
 
-  describe "jsonapi_wrapper" do
+  describe "hses_issue_response" do
+    it "returns the expected issue as a json api response" do
+      expected = FakeData::ApiResponse.hses_issues_response[:data].second
+      actual = FakeData::ApiResponse.hses_issue_response(expected[:id])[:data]
+
+      expect(actual).to eq(expected)
+    end
+  end
+
+  describe "generate_hses_issue_response" do
+    it "returns a JSON-API object with a single complaint" do
+      res = FakeData::ApiResponse.generate_hses_issue_response
+
+      expect(res[:data].keys.length).to be > 0
+    end
+  end
+
+  describe "jsonapi_list_wrapper" do
     it "returns an object with JSON-API top level JSON API" do
-      wrapper = FakeData::ApiResponse.jsonapi_wrapper
+      wrapper = FakeData::ApiResponse.jsonapi_list_wrapper
 
       expect(wrapper[:meta]).to be_a(Hash)
       expect(wrapper[:data]).to be_a(Array)
       expect(wrapper[:links]).to be_a(Hash)
+      expect(wrapper[:included]).to be_a(Array)
+    end
+  end
+
+  describe "jsonapi_details_wrapper" do
+    it "returns an object with JSON-API top level format" do
+      wrapper = FakeData::ApiResponse.jsonapi_details_wrapper
+
+      expect(wrapper[:data]).to be_a(Hash)
+      expect(wrapper[:included]).to be_a(Array)
     end
   end
 end
