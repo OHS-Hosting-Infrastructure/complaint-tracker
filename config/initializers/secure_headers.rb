810802776802b955de2dffbe5d7f65610eb4e894
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-08-31 11:36:39 -0400

    Include hses login as a valid form action

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -2,7 +2,7 @@ SecureHeaders::Configuration.default do |config|
   config.csp = config.csp.merge({
     default_src: %w['self'],
     font_src: %w['self'],
-    form_action: %w['self'],
+    form_action: %W['self' #{Rails.configuration.x.hses.auth_base}],
     frame_ancestors: %w['none'],
     img_src: %w['self' data:],
     script_src: %w['self'],
