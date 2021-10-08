61234de4a2fa0d6ab82a0cccfd9ee2eb40422447
Kimball Bighorse <kbighorse@yahoo.com>
2021-10-07 19:40:52 -0700

    Test Issue # header in table view

--- spec/views/complaints/_complaints_table.html.erb_spec.rb
+++ spec/views/complaints/_complaints_table.html.erb_spec.rb
@@ -2,12 +2,14 @@ require "rails_helper"
 
 RSpec.describe "rendering complaints table" do
   let(:complaint) { Api::FakeData::Complaint.new.data }
+
   it "has a table to display complaints" do
     @complaints = [complaint]
 
     render partial: "complaints/complaints_table"
 
     expect(rendered).to match '<table class="usa-table" aria-describedby="#caption" >'
+    expect(rendered).to match '<th scope=\"col\" role=\"columnheader\">HSES Issue #</th>\n'
   end
 
   it "has the ability to sort by grantee" do
