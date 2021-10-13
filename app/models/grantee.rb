c7d1e4c6ba49fcedbf41cc1392389305c41f9ab0
Kimball Bighorse <kbighorse@yahoo.com>
2021-10-12 17:40:18 -0700

    Define Grantee model

--- app/models/grantee.rb
+++ app/models/grantee.rb
@@ -1,2 +1,21 @@
-class Grantee < ApplicationRecord
+class Grantee
+  attr_reader :id, :attributes
+
+  EVENT_LABELS = {
+    name: 'Name',
+    summary: 'Summary'
+  }.with_indifferent_access.freeze
+
+  def initialize(hses_grantee)
+    @id = hses_grantee[:id]
+    @attributes = hses_grantee[:attributes].with_indifferent_access
+  end
+
+  def name?
+    name.present?
+  end
+
+  def summary
+    attributes["summary"]
+  end
 end
