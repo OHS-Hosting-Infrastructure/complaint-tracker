620042ec6a995f64c25f5b0ab35b0194d8da9c94
Kimball Bighorse <kbighorse@yahoo.com>
2021-10-18 11:20:38 -0700

    Test for name in view

--- spec/views/grantees/show.html.erb_spec.rb
+++ spec/views/grantees/show.html.erb_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe "grantees/show", type: :view do
   let(:grantee) { Grantee.new(Api::FakeData::Grantee.new.data) }
+  let(:name)
 
-  it "renders attributes in <p>" do
-    render
+  it "displays the grantee name in an h1" do
+    expect(rendered).to match "<h1>#{grantee.name}</h1>"
   end
 end
