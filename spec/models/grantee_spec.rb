dc27443261228529da2a64fba5909f85fe8bb8f0
Kimball Bighorse <kbighorse@yahoo.com>
2021-10-18 13:34:30 -0700

    Test fields on grantee

--- spec/models/grantee_spec.rb
+++ spec/models/grantee_spec.rb
@@ -1,4 +1,33 @@
 require "rails_helper"
 
 RSpec.describe Grantee, type: :model do
+  let(:hses_grantee) { Api::FakeData::Grantee.new.data }
+  subject { described_class.new hses_grantee }
+
+  before do
+    hses_grantee[:attributes].merge!({
+    })
+  end
+
+  describe "initialize" do
+    it "sets @id" do
+      expect(subject.id).to eq hses_grantee[:id]
+    end
+
+    it "sets @attributes" do
+      expect(subject.attributes).to eq hses_grantee[:attributes].with_indifferent_access
+    end
+  end
+
+  describe "#name" do
+    it "deletages to the attributes" do
+      expect(subject.name).to eq hses_grantee[:attributes][:name]
+    end
+  end
+
+  describe "#region" do
+    it "deletages to the attributes" do
+      expect(subject.region).to eq hses_grantee[:attributes][:region]
+    end
+  end
 end
