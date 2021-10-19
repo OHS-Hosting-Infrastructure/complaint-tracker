ad810ec14978ffc3efb24305ee00ecf191209d2e
Kimball Bighorse <kbighorse@yahoo.com>
2021-10-19 10:20:42 -0700

    Set grantee in show method

--- app/controllers/grantees_controller.rb
+++ app/controllers/grantees_controller.rb
@@ -1,17 +1,10 @@
 class GranteesController < ApplicationController
-  before_action :set_grantee, only: %i[show]
-
   # GET /grantees/1 or /grantees/1.json
   def show
-    render layout: "details"
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_grantee
     @grantee = Grantee.new(
       Api::FakeData::Grantee.new.data
     )
+
+    render layout: "details"
   end
 end
