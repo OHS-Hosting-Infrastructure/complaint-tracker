f91bc1434be0cf8c79ddd48c7082bf272c298131
Kimball Bighorse <kbighorse@yahoo.com>
2021-10-19 08:44:59 -0700

    Test for and define only grantees show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :grantees
   resources :complaints, only: %i[index show]
+  resources :grantees, only: %i[show]
   delete "issue_tta_report/unlink_report/:issue_id", to: "issue_tta_reports#unlink", as: :unlink_tta_report
   # session pages
   get "/oauth2-client/login/oauth2/code/", to: "sessions#create"
