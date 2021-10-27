e236389387d6435d7eeba346ccdf64114847dad4
Ryan Ahearn <ryan.ahearn@hey.com>
2021-10-27 11:27:35 -0400

    Add join table between issue id and monitoring review id

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_154252) do
+ActiveRecord::Schema.define(version: 2021_10_27_151440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "issue_monitoring_reviews", force: :cascade do |t|
+    t.string "issue_id", null: false
+    t.string "review_id", null: false
+    t.date "start_date", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "issue_tta_reports", force: :cascade do |t|
     t.string "issue_id"
---
+++ spec/models/issue_monitoring_review_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe IssueMonitoringReview, type: :model do
+  subject do
+    described_class.new(
+      issue_id: "ISSUE_ID",
+      review_id: "IT-AMS-ID",
+      start_date: 1.day.ago
+    )
+  end
+
+  describe "validations" do
+    it "is valid with all attributes set" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without an issue id" do
+      subject.issue_id = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "is not valid without a review id" do
+      subject.review_id = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "is not valid without a start date" do
+      subject.start_date = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "is not valid if an identical join model already exists" do
+      described_class.create(subject.attributes)
+      expect(subject).not_to be_valid
+    end
+  end
+end
