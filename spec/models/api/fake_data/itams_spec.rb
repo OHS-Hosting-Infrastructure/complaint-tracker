02544f3fd2e79ee552fc928d648bdf35a5a07be9
JessicaVDussault <jessica.dussault@gsa.gov>
2021-10-27 13:07:54 -0500

    fixes file with wrong name, found other things wrong, too

--- spec/models/api/fake_data/itams_spec.rb
+++ spec/models/api/fake_data/itams_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 RSpec.describe Api::FakeData::Itams do
   describe "Review" do
-    subject { Api::FakeData::Tta::Itams.new(id: id) }
+    subject { Api::FakeData::Itams::Review.new(id: id) }
 
     describe "#request" do
       let(:id) { "1234" }
 
       it "returns a review with the correct id" do
-        expect(subject.request.data[:attributes][:id]).to eq id
+        expect(subject.request.data[:id]).to eq id
       end
 
       it "wraps the review in a detail wrapper" do
