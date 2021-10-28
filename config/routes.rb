d088aaadc639d14020677fc9177d530d4872daff
JessicaVDussault <jessica.dussault@gsa.gov>
2021-10-28 10:03:22 -0500

    copies tta partials and JS for ITAMS
    minimal effort towards combining at this point

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   resources :complaints, only: %i[index show]
   resources :grantees, only: %i[show]
+
+  # complaint links with TTA or IT-AMS
+  resources :issue_tta_reports, only: [:create, :update]
   delete "issue_tta_report/unlink_report/:issue_id", to: "issue_tta_reports#unlink", as: :unlink_tta_report
+  resources :issue_itams_reviews, only: [:create, :update]
+
   # session pages
   get "/oauth2-client/login/oauth2/code/", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-
-  resources :issue_tta_reports, only: [:create, :update]
 
   root to: "pages#welcome"
 end
