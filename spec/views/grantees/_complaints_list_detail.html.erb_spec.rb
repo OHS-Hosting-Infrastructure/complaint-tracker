9954bfccf3d9be897c825d7fedfb20b6b4e74cc0
Kimball Bighorse <kbighorse@yahoo.com>
2021-10-29 10:28:26 -0700

    Move partials with view specs

--- spec/views/grantees/_complaints_list_detail.html.erb_spec.rb
+++ spec/views/grantees/_complaints_list_detail.html.erb_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "rendering complaints list" do
   let(:complaint) { Complaint.new(hses_complaint) }
 
   it "displays a complaint card" do
-    render partial: "complaints/complaints_list_detail", locals: {complaint: complaint}
+    render partial: "grantees/complaints_list_detail", locals: {complaint: complaint}
     complaint_header = "<h2>Complaint ##{complaint.id}</h2>"
     expect(rendered.squish).to match complaint_header
   end
