be38e7dd27a63678053da4b9a6557c380a05c9a7
JessicaVDussault <jessica.dussault@gsa.gov>
2021-10-29 11:20:56 -0500

    caught renaming issue

--- app/controllers/issue_monitoring_reviews_controller.rb
+++ app/controllers/issue_monitoring_reviews_controller.rb
@@ -2,17 +2,17 @@ class IssueMonitoringReviewsController < ApplicationController
   include NeedsHsesAccessToken
 
   def create
-    @issue_monitoring_report = IssueMonitoringReview.new(
+    @issue_monitoring_review = IssueMonitoringReview.new(
       issue_id: issue_id,
       review_id: review_id,
       access_token: hses_access_token
     )
 
     respond_to do |format|
-      if @issue_monitoring_report.save
+      if @issue_monitoring_review.save
         format.js { render inline: "location.reload(true);" }
       else
-        Rails.logger.error("Problem saving the link to RAN review: " + @issue_monitoring_report.errors.full_messages.join("; "))
+        Rails.logger.error("Problem saving the link to RAN review: " + @issue_monitoring_review.errors.full_messages.join("; "))
         # TODO implement when working on error handling
         # format.js { render "create_errors" }
       end
