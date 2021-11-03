c4472ec545825b2bbcf452d037a7bb9024a1d346
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-11-03 16:44:12 -0400

    Pass sort value through to HSES issues endpoint

---
+++ app/javascript/packs/complaint-sort.js
@@ -0,0 +1,13 @@
+import { table } from 'uswds';
+
+const selector = "th[data-sortable]"
+
+document.querySelector(selector).addEventListener('click', event => {
+  event.stopPropagation()
+  
+  const { target: eventTarget } = event
+  const columnTarget = eventTarget.closest(selector)
+  const { nextSort } = columnTarget.dataset
+
+  window.location = nextSort
+})
