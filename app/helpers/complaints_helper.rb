af677031c2693082a851cba916ee5010cd84b203
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-11-04 11:40:15 -0400

    Address PR feedback

--- app/helpers/complaints_helper.rb
+++ app/helpers/complaints_helper.rb
@@ -36,21 +36,33 @@ module ComplaintsHelper
     STATUS_SORT_ORDER[status_label]
   end
 
-  def aria_sort(column)
-    current_sort_direction = current_sort_direction(column)
-    if current_sort_direction.present?
-      "aria-sort=\"#{current_sort_direction}\""
+  def aria_sort(sort_value_for_column)
+    current_sort_direction_for_column = current_sort_direction(sort_value_for_column)
+    if current_sort_direction_for_column.present?
+      "aria-sort=\"#{current_sort_direction_for_column}\""
     end
   end
 
-  def current_sort_direction(column)
-    if @sort.nil?
-      column == "creationDate" ? "descending" : nil
-    else
-      match = @sort.match(/(-)?#{column}/)
+  def current_sort_direction(sort_value_for_column)
+    if @sort.nil? && sort_value_for_column == "creationDate"
+      # Defaults to a descending creation date sort when no sort is specified
+      "descending"
+    elsif @sort.present?
+      match = @sort.match(/(-)?#{sort_value_for_column}/)
+      # if we don't have a match we're sorting by some other column, so return nil from this function
       if match
+        # the current sort direction having a leading `-` means we're sorted descending
         match[1] == "-" ? "descending" : "ascending"
       end
+    end
+  end
+
+  def next_sort_direction(sort_value_for_column)
+    # sort descending (leading `-`) if currently unsorted or sorted ascending
+    if current_sort_direction(sort_value_for_column) == "descending"
+      sort_value_for_column
+    else
+      "-#{sort_value_for_column}"
     end
   end
 end
