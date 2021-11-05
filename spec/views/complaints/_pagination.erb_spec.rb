222b1c8828c41d9cf1a4b5aa707e96dbfdc91aca
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-11-05 13:52:54 -0400

    Add tests for previous and next links

--- spec/views/complaints/_pagination.erb_spec.rb
+++ spec/views/complaints/_pagination.erb_spec.rb
@@ -10,6 +10,20 @@ RSpec.describe "rendering pagination" do
       expect(rendered).to include '<a class="usa-pagination__button usa-current" aria-label="Page 1" aria-current="page" href="/complaints?page=1">1</a>'
     end
 
+    it "generates proper link for previous page" do
+      @pagy = Pagy.new(count: 250, page: 2)
+
+      render partial: "complaints/pagination", locals: {pagy: @pagy, path_args: {sort: "creationDate"}}
+      expect(rendered).to include '<a class="usa-pagination__link usa-pagination__previous-page" aria-label="Previous page" href="/complaints?page=1&amp;sort=creationDate">'
+    end
+
+    it "generates proper link for next page" do
+      @pagy = Pagy.new(count: 250)
+
+      render partial: "complaints/pagination", locals: {pagy: @pagy, path_args: {sort: "creationDate"}}
+      expect(rendered).to include '<a class="usa-pagination__link usa-pagination__next-page" aria-label="Next page" href="/complaints?page=2&amp;sort=creationDate">'
+    end
+
     it "selects appropriate page" do
       @pagy = Pagy.new(count: 250, page: 5)
 
